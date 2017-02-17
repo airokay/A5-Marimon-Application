67fff83ddfa9b19fa485ef9549c226c2af94714e
Jeffrey Lima <jlimasau@ucsd.edu>
2017-02-16 22:59:04 -0800

    YOLO

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
 	initializePage();
+	
 });
-
+var num  = 0;
 function initializePage() {
 	
 $("#sendBtn").click(function(event) {
 		event.preventDefault();
 		var input = $('#inputMessage').val();
+		//var num = 1;
+		if(num == 5)
+			num = 0;
+		num = num +1;
+		var pic = "images/" + num + ".png"
 		var htmlElement = '<div class = "chatbox" > ' +
   				'<div class="answer chat"> '+
   					'<img src="images/Marimon face.png" style= "display:none"> ' +
        				'<span>' + input +' </span> ' +
+       				'<img src=' + pic  + ' style= "img-responsive col-sm-12"> ' +
        			'</div> ' +
-  			'</div>';
+  			'</div>'; 
 		$('#messagecontainer').append(htmlElement);
 	});
 }
